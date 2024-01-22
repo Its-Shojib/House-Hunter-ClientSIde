b1cd900b081bc99fbb0402fa6245b91139d4753b
Its_Shojib <mdshojib922@gmail.com>
2024-01-22 17:20:14 +0600

    Client Side Created

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <>Hello World</>
+    }
+])
+
+
+export default router;
