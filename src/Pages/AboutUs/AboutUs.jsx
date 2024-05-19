90acc7f854d077f4e7090c8ad979a01441ca6b8e
Md Shojib Hossain <mdshojib922@gmail.com>
2024-05-20 00:25:06 +0600

    Code Optimized

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -3,38 +3,47 @@ import Lottie from 'lottie-react';
 
 const AboutUs = () => {
     return (
-        <div className="my-10 pt-1">
+        <div className="w-full md:w-10/12 mx-auto my-10 pt-20">
             <div className="flex flex-col md:flex-row gap-10 items-center">
                 <div className="flex-1">
                     <Lottie className="h-[450px] w-10/12" animationData={animation} loop={true}></Lottie>
                 </div>
 
-                <div className='flex-1'>
+                <div className='flex-1 space-y-1'>
                     <div className="collapse bg-base-200">
                         <input type="radio" name="my-accordion-1" checked="checked" />
                         <div className="collapse-title text-xl font-medium">
-                            Click to open this one and close others
+                            How to create account in the House Hunter?
                         </div>
                         <div className="collapse-content">
-                            <p>hello</p>
+                            <p>Its so simple! Just go to Register and get Registered</p>
                         </div>
                     </div>
                     <div className="collapse bg-base-200">
                         <input type="radio" name="my-accordion-1" />
                         <div className="collapse-title text-xl font-medium">
-                            Click to open this one and close others
+                            How to find perfect House in House Hunter?
                         </div>
                         <div className="collapse-content">
-                            <p>hello</p>
+                            <p>Go to Home Page and Search House!!!</p>
                         </div>
                     </div>
                     <div className="collapse bg-base-200">
                         <input type="radio" name="my-accordion-1" />
                         <div className="collapse-title text-xl font-medium">
-                            Click to open this one and close others
+                            How to Book House in House Hunter?
                         </div>
                         <div className="collapse-content">
-                            <p>hello</p>
+                            <p>{`Tap on any house and Click on "Book this House"`}</p>
+                        </div>
+                    </div>
+                    <div className="collapse bg-base-200">
+                        <input type="radio" name="my-accordion-1" />
+                        <div className="collapse-title text-xl font-medium">
+                            How to manage bookings?
+                        </div>
+                        <div className="collapse-content">
+                            <p>Go to Dashboard and Manage your booked house</p>
                         </div>
                     </div>
                 </div>
